b876e47db019f4f06537d62a08471fcc102740c7
Timofey <pinecarper@icloud.com>
2022-01-25 10:30:00 +0100

    ability: reset cube rotation

--- Assets/Scripts/CubeRotator.cs
+++ Assets/Scripts/CubeRotator.cs
@@ -9,10 +9,14 @@ public class CubeRotator : MonoBehaviour
     private float _rotationSpeed;
     private float _inputX;
     private float _inputY;
+    private float _resetTime;
 
+    public bool _isReset;
+    
     void Start()
     {
         _rotationSpeed = 100.0f;
+        _resetTime = 5f;
     }
 
     void Update()
@@ -20,7 +24,30 @@ public class CubeRotator : MonoBehaviour
         _inputX = Input.GetAxis("Horizontal");
         _inputY = Input.GetAxis("Vertical");
 
-        transform.Rotate(Vector3.down * _inputX * _rotationSpeed * Time.deltaTime, Space.World);
-        transform.Rotate(Vector3.right * _inputY * _rotationSpeed * Time.deltaTime, Space.World);
+        if (_isReset == false)
+        {
+            transform.Rotate(Vector3.down * _inputX * _rotationSpeed * Time.deltaTime, Space.World);
+            transform.Rotate(Vector3.right * _inputY * _rotationSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reset());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isReset)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, _resetTime * Time.deltaTime);
+        }
+    }
+
+    private IEnumerator Reset()
+    {
+        _isReset = true;
+        yield return new WaitForSeconds(1);
+        _isReset = false;
     }
 }
