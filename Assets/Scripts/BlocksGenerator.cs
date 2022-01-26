1b9ce873f79978e20aab2440dd31ecf39d2551e2
Timofey <pinecarper@icloud.com>
2022-01-26 22:10:43 +0100

    first cube level generation

---
+++ Assets/Scripts/BlocksGenerator.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BlocksGenerator : MonoBehaviour
+{
+    public List<GameObject> blocks = new List<GameObject>();
+    public GameObject cube;
+    public GameObject grass;
+    private GameObject _firstBlock;
+    private GameObject _newBlock;
+    private GameObject _firstSpawn;
+
+    public Transform spawnR;
+    public Transform spawnL;
+    public Transform spawnU;
+
+    private SpawnPoint _spawnPoint;
+
+    private int _blocksNumber;
+    private int _linesNumber;
+
+    private void Start()
+    {
+        _blocksNumber = 0;
+        _linesNumber = 0;
+        
+        _firstSpawn = GameObject.FindGameObjectWithTag("SpawnPoint");
+    }
+
+    private void Update()
+    {
+        if (_firstSpawn.GetComponent<SpawnPoint>().isFull == false)
+        {            
+            _firstSpawn.GetComponent<SpawnPoint>().isFull = true;
+            _newBlock = Instantiate(grass, _firstSpawn.transform.position, Quaternion.identity);
+            _newBlock.name = "Grass" + _blocksNumber;
+            spawnL = _newBlock.transform.Find("Spawn Point Left");
+            
+            _blocksNumber++;
+            
+            blocks.Add(_newBlock);
+            _newBlock.transform.parent = cube.transform;
+            
+            _firstBlock = _newBlock;
+        }
+        
+        else
+        {
+            if (_linesNumber < 10)
+            {
+                if (spawnL.GetComponent<SpawnPoint>().isFull == false && blocks.Count % 10 != 0)
+                {
+                    
+                    spawnL.GetComponent<SpawnPoint>().isFull = true;
+                    _newBlock = Instantiate(grass, spawnL.transform.position, Quaternion.identity);
+                    spawnL = _newBlock.transform.Find("Spawn Point Left");
+                    _newBlock.name = "Grass " + _blocksNumber;
+
+                    _blocksNumber++;
+                    
+                    blocks.Add(_newBlock);
+                    _newBlock.transform.parent = cube.transform;
+                }
+            }
+            
+            if (blocks.Count % 10 == 0 && blocks.Count % 100 != 0)
+            {
+                spawnL.GetComponent<SpawnPoint>().isFull = true;
+                _firstBlock = Instantiate(grass, _firstBlock.transform.Find("Spawn Point Right").transform.position, Quaternion.identity);
+                spawnL = _firstBlock.transform.Find("Spawn Point Left");
+                _firstBlock.name = "Grass " + _blocksNumber;
+
+                _linesNumber++;
+                _blocksNumber++;
+                
+                blocks.Add(_firstBlock);
+                _firstBlock.transform.parent = cube.transform;
+            }
+
+            if (blocks.Count % 100 == 0)
+            {
+                _newBlock.transform.Find("Spawn Point Right").GetComponent<SpawnPoint>().isFull = true;
+            }
+            
+        }
+        
+        Debug.Log(_blocksNumber);
+    }
+}
